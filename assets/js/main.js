fe84a89841350e5d6fab01bf91b8ee721f058176
unknown <essatenilas@gmail.com>
2023-11-08 23:18:11 +0200

    add: adaptive

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    speed: 900,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    }
+    /* pagination: {
+      el: ".swiper-pagination",
+      clickable: false,
+    }, */
+    /* navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }, */
+  });
+
