905c194c3dd25776cf76777ac21baac399cc7ee7
unknown <essatenilas@gmail.com>
2023-11-11 23:45:14 +0200

    feat adaptive for 768px

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,8 @@ var swiper = new Swiper(".mySwiper", {
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
-    /* pagination: {
-      el: ".swiper-pagination",
-      clickable: false,
-    }, */
-    /* navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    }, */
+    },
   });
+
+
 
